526956814e421f9f9bf03cb7d1f687b42dd361b2
AgadagbaEsther <agadagbaesther@gmail.com>
2026-05-19 14:36:32 +0100

    fix: update vite base path config

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/react-swc' // (Leave your standard framework imports as they are!)
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Nexus-UNIMED/',
+  plugins: [react()], // (Leave your plugins array as it is!)
+  base: '/Nexus-UNIMED-project/', // <--- THIS LINE IS CRITICAL!
 })
