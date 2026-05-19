d627e891f0a1be2aecda36253b5a7896d291038c
AgadagbaEsther <agadagbaesther@gmail.com>
2026-05-19 14:55:30 +0100

    refinalise

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-swc' // (Leave your standard framework imports as they are!)
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()], // (Leave your plugins array as it is!)
-  base: '/Nexus-UNIMED-project/', // <--- THIS LINE IS CRITICAL!
+  base: './', // <--- Change this to a dot and a slash!
 })
