2b8e4ad8fd89c793fb7aef46ac8d793a193b0bc6
AgadagbaEsther <agadagbaesther@gmail.com>
2026-05-21 16:08:10 +0100

    config

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/Nexus-UNIMED-project/', // <--- Change this to a dot and a slash!
+  base: './', // <--- Change this to a dot and a slash!
 })
